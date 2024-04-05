397e481a4772cf380acf1b90fa71520fdb6c8d67
akdl911215 <akdl911215@naver.com>
2024-04-05 14:21:00 +0900

    token

---
+++ src/users/infrastructure/token/dtos/generate.token.dto.ts
@@ -0,0 +1,30 @@
+import { IsOptional, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import * as diagnostics_channel from 'diagnostics_channel';
+
+export class GenerateTokenInputDto {
+  @IsString()
+  @IsOptional()
+  @ApiProperty({
+    description: 'access-token',
+    type: String,
+    required: false,
+    default: '',
+  })
+  public readonly accessToken?: string;
+
+  @IsString()
+  @IsOptional()
+  @ApiProperty({
+    description: 'refresh-token',
+    type: String,
+    required: false,
+    default: '',
+  })
+  public readonly refreshToken?: string;
+}
+
+export type GenerateTokenOutputDto = {
+  readonly accessToken: string;
+  readonly refreshToken: string;
+};
