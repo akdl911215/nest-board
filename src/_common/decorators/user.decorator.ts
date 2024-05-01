f9b9f1255472b93df6b99d13a739e2cf7d24d858
akdl911215 <akdl911215@naver.com>
2024-05-01 18:05:28 +0900

    user decorator

--- src/_common/decorators/user.decorator.ts
+++ src/_common/decorators/user.decorator.ts
@@ -4,6 +4,6 @@ export const User = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
 
-    return request;
+    return request?.user;
   },
 );
