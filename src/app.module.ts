f6843f6c4ab21eed07ccee5d974d66577c19b3ef
akdl911215 <akdl911215@naver.com>
2024-05-05 14:54:45 +0900

    pakage.json && configModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,11 @@ import { UsersModule } from './users/users.module';
 import { RepliesModule } from './replies/replies.module';
 import { CommentsModule } from './comments/comments.module';
 import { ReactionsModule } from './reactions/reactions.module';
+import { CONFIG_MODULE } from './_common/infrastructure/env';
 
 @Module({
   imports: [
+    CONFIG_MODULE,
     ConfigModule,
     BoardsModule,
     UsersModule,
