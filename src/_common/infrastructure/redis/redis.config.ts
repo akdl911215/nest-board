dd48e47e70ac3d167bb19e336a8cb1434fa30fe5
akdl911215 <akdl911215@naver.com>
2024-05-29 20:05:42 +0900

    5....

--- src/_common/infrastructure/redis/redis.config.ts
+++ src/_common/infrastructure/redis/redis.config.ts
@@ -1,15 +1,17 @@
 import { RedisModule } from '@liaoliaots/nestjs-redis';
 
-const ENV = process.env.NODE_ENV === 'production'
+const HOST = process.env.NODE_ENV === 'production'
   ? '3.35.207.45'
   : '127.0.0.1'
+console.log('redis config HOST : ', HOST);
 
 const PORT: number = parseInt(process.env.REDIS_PORT);
+console.log('redis config PORT : ', PORT);
 
 export const REDIS_MODULE = RedisModule.forRoot({
   readyLog: true,
   config: {
-    host: ENV,
+    host: HOST,
     port: PORT,
     // password: '',
   },
