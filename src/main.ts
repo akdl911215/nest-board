b3ffe6b88bc63c428cf7e99f6a4e008cf20ef3f5
akdl911215 <akdl911215@naver.com>
2024-05-29 17:00:57 +0900

    1....

--- src/main.ts
+++ src/main.ts
@@ -58,7 +58,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
-  //
+  ////
   await app.listen(PORT, () => console.log(`http://${HOST}:${PORT}/docs`));
 }
 bootstrap().then();
