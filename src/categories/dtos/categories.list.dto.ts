facce34a2006816176054096310ca7727af8e520
akdl911215 <akdl911215@naver.com>
2024-06-09 22:43:44 +0900

    Remove Category

--- src/categories/dtos/categories.list.dto.ts
+++
@@ -1,9 +0,0 @@
-import {
-  BaseOffsetPaginationInputDto,
-  BaseOffsetPaginationOutputDto,
-} from '../../_common/abstract/base.pagination.dto';
-import { Categories } from '@prisma/client';
-
-export class CategoriesListInputDto extends BaseOffsetPaginationInputDto {}
-
-export class CategoriesListOutputDto extends BaseOffsetPaginationOutputDto<Categories> {}
