cd4c8c55dd08b4c346e1f44a6b1d39115e1a9e6d
akdl911215 <akdl911215@naver.com>
2024-06-11 12:27:34 +0900

    Modify module in Board Tags

--- src/board.tags/board.tags.module.ts
+++ src/board.tags/board.tags.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { BoardTagsService } from './board.tags.service';
 import { BoardTagsController } from './board.tags.controller';
+import { PrismaService } from '../_common/infrastructure/prisma.service';
+import { BoardTagsRepository } from './board.tags.repository';
 
 @Module({
   controllers: [BoardTagsController],
-  providers: [BoardTagsService],
+  providers: [
+    // infrastructure
+    PrismaService,
+
+    // service
+    { provide: 'SERVICE', useClass: BoardTagsService },
+
+    // repository
+    { provide: 'REPOSITORY', useClass: BoardTagsRepository },
+  ],
 })
 export class BoardTagsModule {}
